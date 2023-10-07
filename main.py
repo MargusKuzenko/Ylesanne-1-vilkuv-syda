fd5052bee0884c05c303c5339ea0200a62046377
MargusKuzenko <116038628+MargusKuzenko@users.noreply.github.com>
2023-10-07 20:01:28 +0300

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,10 @@
+def on_forever():
+    basic.show_icon(IconNames.HEART)
+    basic.show_leds("""
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        """)
+basic.forever(on_forever)
